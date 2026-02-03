7e9637104f4d0576d8fe5cfb867fdeaf9c34c857
25ats11 <yip_yu_chen_aiden@students.edu.sg>
2026-02-03 15:18:46 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    Counter += 1
+    basic.showNumber(Counter)
+})
+input.onButtonPressed(Button.B, function () {
+    Counter += -1
+    basic.showNumber(Counter)
+})
+let Counter = 0
+Counter = 0
+basic.showNumber(0)
